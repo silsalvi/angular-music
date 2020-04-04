2a7a99a2fa69fbd5822430cf1f43e65572fe0005
Silvestro <silvestro@lenovo-g50-70>
2020-04-04 10:34:55 +0200

    Implementato servizio getBrani in rest api service

--- angular-music/src/app/app.module.ts
+++ angular-music/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomePlayerComponent } from "./home-player/home-player.component";
-import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
+import { LoadingScreenComponent } from "./loading-screen/loading-screen.component";
 
 @NgModule({
   declarations: [AppComponent, HomePlayerComponent, LoadingScreenComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- angular-music/src/app/services/rest-api.service.ts
+++ angular-music/src/app/services/rest-api.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Brano } from "../model/brano";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RestApiService {
-
-  constructor() { }
+  private url = "./assets/app-data/brani.json";
+  constructor(private http: HttpClient) {}
+  /**
+   * recupera i brani
+   */
+  getBrani() {
+    return this.http.get<Brano[]>(this.url);
+  }
 }
